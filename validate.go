a4abbbb704d992c6a780b0766c6ecd7e6e70f9db
Joakim Lundborg <lundborg@wrapp.com>
2015-06-29 16:15:34 +0200

    Implement streaming JSON validation

--- validate.go
+++ validate.go
@@ -33,6 +33,9 @@ func (nopCloser) Close() error { return nil }
 
 // ValidateJSONSchema returns a http middleware that validates the supplied
 // JSON schema. Will panic if the schema file can't be found and/or is invalid
+// The validator supports streaming JSON, and will buffer all data to check its
+// validity. If any of the streamed objects is invalid, subsequent handlers will
+// not be called.
 func ValidateJSONSchema(path string) func(http.Handler) http.Handler {
 	schemaLoader := gojsonschema.NewReferenceLoader("file://" + path)
 	schema, err := gojsonschema.NewSchema(schemaLoader)
@@ -46,16 +49,30 @@ func ValidateJSONSchema(path string) func(http.Handler) http.Handler {
 				http.Error(w, "Failed to read body: "+err.Error(), http.StatusBadRequest)
 				return
 			}
-			validationResult, err := schema.Validate(gojsonschema.NewStringLoader(string(buf)))
-			if err != nil {
-				http.Error(w, "Failed to validate: "+err.Error(), http.StatusInternalServerError)
-				return
+			bufReader := bytes.NewReader(buf)
+			dec := json.NewDecoder(bufReader)
+
+			for {
+				var obj interface{}
+				if err := dec.Decode(&obj); err == io.EOF {
+					break
+				} else if err != nil {
+					http.Error(w, "Invalid JSON: "+err.Error(), http.StatusBadRequest)
+				}
+
+				objLoader := gojsonschema.NewGoLoader(obj)
+				validationResult, err := schema.Validate(objLoader)
+				if err != nil {
+					http.Error(w, "Failed to validate: "+err.Error(), http.StatusInternalServerError)
+					return
+				}
+				if !validationResult.Valid() {
+					validationResultJSON, _ := json.Marshal(ValidationResult{*validationResult})
+					http.Error(w, string(validationResultJSON), http.StatusBadRequest)
+					return
+				}
 			}
-			if !validationResult.Valid() {
-				validationResultJSON, _ := json.Marshal(ValidationResult{*validationResult})
-				http.Error(w, string(validationResultJSON), http.StatusBadRequest)
-				return
-			}
+
 			r.Body = nopCloser{bytes.NewReader(buf)}
 			next.ServeHTTP(w, r)
 		})
